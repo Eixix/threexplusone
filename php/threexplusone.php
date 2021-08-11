d8d66c7be405ec27ab0f2ed8589f867f9e185316
Tobias <tobias.betz@uni-ulm.de>
2021-08-11 21:37:00 +0200

    Initial commit

---
+++ php/threexplusone.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+function threexplusone($x) 
+{
+	if ($x == 1) {
+		return [1];
+	} else if ($x % 2 == 1) {
+		return array_merge(threexplusone(3 * $x + 1), [$x]);
+	} else {
+		return array_merge(threexplusone($x / 2), [$x]);
+	}
+}
+
+print_r(array_map('threexplusone', range(1, 1000)));
